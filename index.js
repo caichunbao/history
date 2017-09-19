fd13fd12c49cd9e5b08911bc96f47dcdea4f98f0
caichunbao <vipcaichunbao@163.com>
2017-09-19 19:02:12 +0800

    add index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+/**
+ * HTML5 history API
+ * Author:caichunbao
+ * Date:2017-9-19
+ * 
+ */
+
+/**
+ * 后退一步
+ * window.history.back()
+ * 
+ * 前进一步
+ * window.history.forword()
+ * 
+ * 后退一步
+ * window.history.go(-1)
+ * 
+ * 后退两步
+ * window.history.go(-2)
+ * 
+ * 前进两步
+ * window.history.go(2)
+ * 
+ */
